725bf69f6745d34596a2a7487c59dc6f8b812f81
Cosimo Sguanci <cosimosguanci@gmail.com>
2022-11-22 18:39:53 -0600

    Update datasets

--- blocks/blocks-parser.py
+++ blocks/blocks-parser.py
@@ -9,11 +9,13 @@ blocks_json_array = []
 # `index` directory (LevelDB index) being maintained by bitcoind. It contains
 # .ldb files and is present inside the `blocks` directory.
 blockchain = Blockchain(os.path.expanduser('.bitcoin/blocks'))
-for block in blockchain.get_ordered_blocks(os.path.expanduser('.bitcoin/blocks/index'), start=497928, end=720000):
+for block in blockchain.get_ordered_blocks(os.path.expanduser('.bitcoin/blocks/index'), start=498084, end=763821):
     print("height=%d block=%s len=%d" % (block.height, block.hash, block.n_transactions))
     b = {}
     b["height"] = block.height
     b["n_transactions"] = block.n_transactions
+    header = block.header
+    b["timestamp"] = header.timestamp.timestamp()
     blocks_json_array.append(b)
 
 with open('blocks.json', 'w') as outfile:
